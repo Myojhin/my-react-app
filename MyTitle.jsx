6275f7ac8a68e2c5be03c08dbeae763bb062b536
ALIX CHAMPAGNOL <alix.champagnol@gmail.com>
2024-06-11 12:02:42 +0200

    ajoutComponent.1

---
+++ MyTitle.jsx
@@ -0,0 +1,18 @@
+// function MyTitle() {
+//     return <h1>Hello React</h1>;
+//   }
+  
+//   export default MyTitle;
+
+// import MyTitle from "./components/MyTitle"
+
+// function App() {
+//   return (
+//     <div>
+//       <MyTitle />
+//     </div>
+//   );
+// }
+
+// export default App;
+  
